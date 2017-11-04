914e6e05b94aa5d01c126308aa8ef0ac2a4c4b75
Terence Stamp <terence.stamp@capgemini.co.uk>
2017-11-04 00:15:32 +0000

    replication

--- opendj-node2/bootstrap/setup.sh
+++ opendj-node2/bootstrap/setup.sh
@@ -12,6 +12,11 @@ if [ -n "${NUMBER_SAMPLE_USERS+set}" ]; then
     INIT_OPTION="--sampleData ${NUMBER_SAMPLE_USERS}"
 fi
 
+secondary=`hostname -f`
+primary=`echo $secondary | sed 's/node2/node1/g'`
+remote_node=$primary
+local_node=$secondary
+
 # todo: We may want to specify a keystore using --usePkcs12keyStore, --useJavaKeystore
 /opt/opendj/setup -p 1389 --ldapsPort 1636 --enableStartTLS  \
   --adminConnectorPort 4444 \
@@ -29,3 +34,40 @@ if [ -d /opt/opendj/bootstrap/ldif ]; then
       /opt/opendj/bin/ldapmodify -D "cn=Directory Manager" -h localhost -p 1389 -w ${PASSWORD} -f /tmp/file.ldif
   done
 fi
+
+# Configuring Replication
+echo "Enabling Replication..."
+/opt/opendj/bin/dsreplication \
+configure \
+--adminUID admin \
+--adminPassword password \
+--baseDN $BASE_DN \
+--host1 $primary \
+--port1 4444 \
+--bindDN1 "cn=Directory Manager" \
+--bindPassword1 ${PASSWORD} \
+--replicationPort1 8989 \
+--secureReplication1 \
+--host2 $secondary \
+--port2 4444 \
+--bindDN2 "cn=Directory Manager" \
+--bindPassword2 ${PASSWORD} \
+--replicationPort2 8989 \
+--secureReplication2 \
+--trustAll \
+--no-prompt
+
+echo "Replication Enabled."
+echo "Initialising replication..."
+
+/opt/opendj/bin/dsreplication \
+initialize-all \
+--hostname $local_node \
+--port 4444 \
+--adminUID admin \
+--adminPassword password \
+--baseDN $BASE_DN \
+--trustAll \
+--no-prompt
+
+echo "Replication initialized"
